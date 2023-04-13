7149aa52399673e2ce4edb68637e843b9deaf268
AnaSophia1911 <anasophiajacintosouza@gmail.com>
2023-04-13 14:49:27 -0300

    estilização em css

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+        <h1 className='titulo'>Teste</h1>
+        <h2 className='texto'>Teste 2</h2>
+        </>
+    );
+}
+
+export default Home;
