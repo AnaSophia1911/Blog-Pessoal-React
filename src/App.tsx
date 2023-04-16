b2b891bbc203d4cb93c981aeff6659a78bb8cd3a
AnaSophia1911 <anasophiajacintosouza@gmail.com>
2023-04-16 19:34:13 -0300

    navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import Home from './paginas/home/Home'
+import Navbar from './components/statics/navbar/Navbar'
+import Footer from './components/statics/footer/Footer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-<Home />
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   )
 }
 
