25baea7064dba8beb7c10fc95caf102d50b7661b
AnaSophia1911 <anasophiajacintosouza@gmail.com>
2023-04-17 10:52:05 -0300

    modified:   src/App.tsx 	modified:   src/paginas/home/Home.css 	modified:   src/paginas/login/Login.tsx

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import {Grid, Typography, Button } from '@material-ui/core';
+import {Link} from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import './Login.css'
 
@@ -13,10 +14,13 @@ export default function Login() {
             <Typography variant='h3' gutterBottom color="textPrimary" component='h3' align='center' style={{fontWeight: 'bold'}}>Login</Typography>
             <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth></TextField>
             <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' fullWidth></TextField>
+            <Link to='/home'>
             <Button variant="contained" >Login</Button>
+            </Link>
         </form>
         </Box>
         </Grid>
       </Grid>
+      
       )
 }
